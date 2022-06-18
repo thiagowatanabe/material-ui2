00454f33ead7066e4ee10578e2afc43d43f89ad6
Thiago Watanabe <thiagowatanabe1@hotmail.com>
2022-06-18 11:13:37 -0300

    Adicionado react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-export const  App = () => {
-  return (
-    <div className="App">
-      Teste
-    </div>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+export const App = () => {
+	return (
+		<BrowserRouter>
+			<AppRoutes />
+		</BrowserRouter>
+	);
+};
